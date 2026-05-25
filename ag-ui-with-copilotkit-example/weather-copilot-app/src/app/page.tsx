137ae90ed5eaf42b62887747c1353a5df617b63c
Uyen Ho <uyen.ho@asnet.com.vn>
2026-05-25 11:49:25 +0700

    feat: integrate with copilotkit

---
+++ ag-ui-with-copilotkit-example/weather-copilot-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+import { CopilotChat } from "@copilotkit/react-ui";
+import { CopilotKit } from "@copilotkit/react-core";
+import "@copilotkit/react-ui/styles.css";
+
+export default function Home() {
+  return (
+    <CopilotKit runtimeUrl="http://localhost:4111/chat" agent="weatherAgent">
+      <CopilotChat
+        labels={{
+          title: "Weather Agent",
+          initial: "Hi! 👋 Ask me about the weather, forecasts, and climate.",
+        }}
+      />
+    </CopilotKit>
+  );
+}
